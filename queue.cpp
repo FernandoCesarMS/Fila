c917e29b4b2069c21195f2790d479af34e5656a9
Fernando Cesar <cmnando.silva@gmail.com>
2021-03-23 15:46:42 -0300

    program was done

--- queue.cpp
+++ queue.cpp
@@ -1,65 +1,82 @@
 #include "queue.h"
 
-struct Node {
+struct Node
+{
   int key;
-  Node* next;
+  Node *next;
 };
 
-Queue::Queue() {
+Queue::Queue()
+{
   this->front_ = nullptr;
   this->back_ = nullptr;
 }
 
-void Queue::push(int k) {
-  if (this->count() == 0){
-    Node* x = new Node;
+void Queue::push(int k)
+{
+  if (this->count() == 0)
+  {
+    Node *x = new Node;
     x->key = k;
     x->next = nullptr;
     this->front_ = x;
     this->back_ = x;
   }
-  else{
-    Node* x = new Node{k,nullptr};
+  else
+  {
+    Node *x = new Node{k, nullptr};
     this->back_->next = x;
     this->back_ = x;
   }
 }
 
-void Queue::pop() {
-  if (this->count() == 0){
+void Queue::pop()
+{
+  if (this->count() == 0)
+  {
     throw EmptyException();
   }
-  else if (this->count() == 1){
+  else if (this->count() == 1)
+  {
     this->front_ = this->back_ = nullptr;
   }
-  else{
-    Node* x = new Node{this->front_->next->key,this->front_->next->next};
+  else
+  {
+    Node *x = new Node{this->front_->next->key, this->front_->next->next};
     this->front_ = x;
   }
 }
 
-int Queue::front() const {
-  if (this->count() == 0){
-    throw EmptyException();  
+int Queue::front() const
+{
+  if (this->count() == 0)
+  {
+    throw EmptyException();
   }
-  else{
+  else
+  {
     return this->front_->key;
   }
 }
 
-int Queue::back() const {
-  if (this->count()== 0){
-    throw EmptyException();  
+int Queue::back() const
+{
+  if (this->count() == 0)
+  {
+    throw EmptyException();
   }
-  else{
-    return this->back_->key;   
+  else
+  {
+    return this->back_->key;
   }
 }
 
-int Queue::count() const {
+int Queue::count() const
+{
   Node *end = this->front_;
   int tamanho = 0;
-  while (end != nullptr){
+  while (end != nullptr)
+  {
     tamanho++;
     end = end->next;
   }
